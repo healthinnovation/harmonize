3d9c6177c169aa58a79f1fb4915d0930eb5896c3
Antony Marcos Barja Ingaruca <antony.barja8@gmail.com>
2024-01-09 08:45:59 -0500

    updated

--- up_links.R
+++ up_links.R
@@ -3,55 +3,172 @@ library(tidyverse)
 library(sf)
 ee_Initialize(user = "barja.geografo.pe",drive = T)
 
-hex <- st_read("data/flying_mission_1km2.gpkg") %>%
-  st_geometry() |> 
-  sf_as_ee() |> 
+# 1. Reading community hexagons -------------------------------------------
+
+llanchama_fc <- st_read("data/community_hexagons_of_1km2.gpkg") %>% 
+  filter(village %in% 'LLANCHAMA') %>% 
+  sf_as_ee() %>% 
   ee$FeatureCollection$geometry()
 
-sunga <- st_read("data/zunga.kml") %>% 
-  sf_as_ee()
+docedeabril_fc <- st_read("data/community_hexagons_of_1km2.gpkg") %>% 
+  filter(village %in% '12 DE ABRIL') %>% 
+  sf_as_ee() %>% 
+  ee$FeatureCollection$geometry()
 
-quisto <- st_read("data/quistococha.kml") %>% 
-  sf_as_ee()
+febrero_fc <- st_read("data/community_hexagons_of_1km2.gpkg") %>% 
+  filter(village %in% '23 DE FEBRERO') %>% 
+  sf_as_ee() %>% 
+  ee$FeatureCollection$geometry()
 
-doce_abril <- ee$Image("users/bryanfernandezc/harmonize_piloto/12_abril_rgb")$clip(hex)
-veinte_3 <- ee$Image("users/bryanfernandezc/harmonize_piloto/23_febrero_marzo")$clip(hex)
-cahuide <- ee$Image("users/bryanfernandezc/harmonize_piloto/cahuide")$
-  select(c("b1","b2","b3"))$clip(hex)
-llanchama <- ee$Image("users/bryanfernandezc/harmonize_piloto/llanchama_marzo_2023")$
-  select(c("b1","b2","b3"))$clip(hex)
-paujil <- ee$Image("users/bryanfernandezc/harmonize_piloto/paujil")$clip(hex)
-quistococha <- ee$Image("users/bryanfernandezc/harmonize_piloto/quistococha")$clip(quisto)
-san_carlos <- ee$Image("users/bryanfernandezc/harmonize_piloto/san_carlos")$clip(hex)
-san_lucas <- ee$Image("users/bryanfernandezc/harmonize_piloto/san_lucas")$clip(hex)
-varillal_01 <- ee$Image("users/bryanfernandezc/harmonize_piloto/varillal_zona_01")$clip(hex)
-zungaro <- ee$Image("users/bryanfernandezc/harmonize_piloto/zungarococha")$
-  select(c("b1","b2","b3"))$clip(sunga)
+quistococha_fc <- st_read("data/community_hexagons_of_1km2.gpkg") %>% 
+  filter(village %in% 'QUISTOCOCHA') %>% 
+  sf_as_ee() %>% 
+  ee$FeatureCollection$geometry()
 
-piloto <- ee$ImageCollection(
-  list(doce_abril,veinte_3,llanchama,paujil,quistococha,
-  san_carlos,san_lucas,varillal_01,zungaro,cahuide
-  ))
+sancarlos_fc <- st_read("data/community_hexagons_of_1km2.gpkg") %>% 
+  filter(village %in% 'SAN CARLOS') %>% 
+  sf_as_ee() %>% 
+  ee$FeatureCollection$geometry()
 
-termal <- ee$ImageCollection("users/bryanfernandezc/harmonize-thermal-may2023")$
-  map(function(x){x$clip(hex)}) |> 
+elpaujil_fc <- st_read("data/community_hexagons_of_1km2.gpkg") %>% 
+  filter(village %in% 'EL PAUJIL') %>% 
+  sf_as_ee() %>% 
+  ee$FeatureCollection$geometry()
+
+sanlucas_fc <- st_read("data/community_hexagons_of_1km2.gpkg") %>% 
+  filter(village %in% 'SAN LUCAS') %>% 
+  sf_as_ee() %>% 
+  ee$FeatureCollection$geometry()
+
+zungarococha_fc <- st_read("data/community_hexagons_of_1km2.gpkg") %>% 
+  filter(village %in% 'ZUNGAROCOCHA') %>% 
+  sf_as_ee() %>% 
+  ee$FeatureCollection$geometry()
+
+cahuide01_fc <- st_read("data/community_hexagons_of_1km2.gpkg") %>% 
+  filter(village %in% 'CAHUIDE 01') %>% 
+  sf_as_ee() %>% 
+  ee$FeatureCollection$geometry()
+
+cahuide02_fc <- st_read("data/community_hexagons_of_1km2.gpkg") %>% 
+  filter(village %in% 'CAHUIDE 02') %>% 
+  sf_as_ee() %>% 
+  ee$FeatureCollection$geometry()
+
+cahuide03_fc <- st_read("data/community_hexagons_of_1km2.gpkg") %>% 
+  filter(village %in% 'CAHUIDE 03') %>% 
+  sf_as_ee() %>% 
+  ee$FeatureCollection$geometry()
+
+varillal01_fc <- st_read("data/community_hexagons_of_1km2.gpkg") %>% 
+  filter(village %in% 'EL VARILLAL 01') %>% 
+  sf_as_ee() %>% 
+  ee$FeatureCollection$geometry()
+
+varillal02_fc <- st_read("data/community_hexagons_of_1km2.gpkg") %>% 
+  filter(village %in% 'EL VARILLAL 02') %>% 
+  sf_as_ee() %>% 
+  ee$FeatureCollection$geometry()
+
+varillal03_fc <- st_read("data/community_hexagons_of_1km2.gpkg") %>% 
+  filter(village %in% 'EL VARILLAL 03') %>% 
+  sf_as_ee() %>% 
+  ee$FeatureCollection$geometry()
+
+# 2. Reading ImageCollection from GEE -------------------------------------
+
+llanchama_img   <- ee$Image('users/labsaf/harmonize-rgb/llanchama')$clip(llanchama_fc)
+docedeabril_img <- ee$Image('users/labsaf/harmonize-rgb/12abril')$clip(docedeabril_fc)
+febrero_img     <- ee$Image('users/labsaf/harmonize-rgb/23febrero')$clip(febrero_fc)
+quistococha_img <- ee$Image('users/labsaf/harmonize-rgb/quistococha')$clip(quistococha_fc)
+sancarlos_img   <- ee$Image('users/labsaf/harmonize-rgb/sancarlos')$clip(sancarlos_fc)
+elpaujil_img    <- ee$Image('users/labsaf/harmonize-rgb/elpaujil')$clip(elpaujil_fc)
+sanlucas_img     <- ee$Image('users/labsaf/harmonize-rgb/sanlucas')$clip(sanlucas_fc)
+zungarococha_img <- ee$Image('users/labsaf/harmonize-rgb/zungarococha')$clip(zungarococha_fc)
+cahuide01_img  <- ee$Image('users/labsaf/harmonize-rgb/cahuide02')$clip(cahuide01_fc)
+cahuide02_img  <- ee$Image('users/labsaf/harmonize-rgb/cahuide03')$clip(cahuide02_fc)
+cahuide03_img  <- ee$Image('users/labsaf/harmonize-rgb/cahuide01')$clip(cahuide03_fc)
+varillal01_img <- ee$Image('users/labsaf/harmonize-rgb/varillalzona1')$clip(varillal01_fc) 
+varillal02_img <- ee$Image('users/labsaf/harmonize-rgb/varillalzona2')$clip(varillal02_fc)
+varillal03_img <- ee$Image('users/labsaf/harmonize-rgb/varillalzona3')$clip(varillal03_fc)
+
+
+llanchama_thermal   <- ee$Image('users/bryanfernandezc/harmonize-thermal-may2023/llanchama_termal')$select('b1')$clip(llanchama_fc)
+docedeabril_thermal <- ee$Image('users/bryanfernandezc/harmonize-thermal-may2023/12abril_termal')$select('b1')$clip(docedeabril_fc)
+febrero_thermal     <- ee$Image('users/bryanfernandezc/harmonize-thermal-may2023/23defebrero_termal')$select('b1')$clip(febrero_fc)
+quistococha_thermal <- ee$Image('users/bryanfernandezc/harmonize-thermal-may2023/quistococha_termal')$select('b1')$clip(quistococha_fc)
+sancarlos_thermal   <- ee$Image('users/bryanfernandezc/harmonize-thermal-may2023/sancarlos_termal')$select('b1')$clip(sancarlos_fc)
+elpaujil_thermal    <- ee$Image('users/bryanfernandezc/harmonize-thermal-may2023/elpaujil_termal')$select('b1')$clip(elpaujil_fc)
+sanlucas_thermal     <- ee$Image('users/bryanfernandezc/harmonize-thermal-may2023/sanlucas_termal')$select('b1')$clip(sanlucas_fc)
+zungarococha_thermal <- ee$Image('users/bryanfernandezc/harmonize-thermal-may2023/zungarococha_termal')$select('b1')$clip(zungarococha_fc)
+cahuide01_thermal  <- ee$Image('users/bryanfernandezc/harmonize-thermal-may2023/cahuide_termal_02')$select('b1')$clip(cahuide01_fc)
+cahuide02_thermal  <- ee$Image('users/bryanfernandezc/harmonize-thermal-may2023/cahuide_termal_03')$select('b1')$clip(cahuide02_fc)
+cahuide03_thermal  <- ee$Image('users/bryanfernandezc/harmonize-thermal-may2023/chauide_termal_01')$select('b1')$clip(cahuide03_fc)
+varillal01_thermal <- ee$Image('users/bryanfernandezc/harmonize-thermal-may2023/varillal_termal_01')$select('b1')$clip(varillal01_fc) 
+varillal02_thermal <- ee$Image('users/bryanfernandezc/harmonize-thermal-may2023/varillal_termal_02')$select('b1')$clip(varillal02_fc)
+varillal03_thermal <- ee$Image('users/bryanfernandezc/harmonize-thermal-may2023/varillal_termal_03')$select('b1')$clip(varillal03_fc)
+
+rgb <- ee$ImageCollection(
+  list(
+    llanchama_img,
+    docedeabril_img,
+    febrero_img,
+    quistococha_img,
+    sancarlos_img,
+    elpaujil_img,
+    sanlucas_img,
+    zungarococha_img,
+    cahuide01_img,
+    cahuide02_img,
+    cahuide03_img,
+    varillal01_img,
+    varillal02_img,
+    varillal03_img)
+  ) %>%  
   Map$addLayers()
 
 
-rgb <- Map$addLayers(piloto)
-new_links <- tibble(
-  villages = rgb$rgee$name,
-  rgb = rgb$rgee$tokens,
-  termal = termal$rgee$tokens[c(1:3,6:11,14)]
-  ) %>%
-  mutate(villages = str_to_upper(case_when(
-    villages == "12_abril_rgb" ~ "12 de abril",
-    villages == "23_febrero_marzo" ~ "23 de febrero",
-    villages == "llanchama_marzo_2023" ~ "llanchama",
-    villages == "san_carlos" ~ "san carlos",
-    villages == "san_lucas" ~ "san lucas",
-    villages == "varillal_zona_01" ~ "el varillal",
-    TRUE ~ villages)
-  ))
+thermal <- ee$ImageCollection(
+  list(
+    llanchama_thermal,
+    docedeabril_thermal,
+    febrero_thermal,
+    quistococha_thermal,
+    sancarlos_thermal,
+    elpaujil_thermal,
+    sanlucas_thermal,
+    zungarococha_thermal,
+    cahuide01_thermal,
+    cahuide02_thermal,
+    cahuide03_thermal,
+    varillal01_thermal,
+    varillal02_thermal,
+    varillal03_thermal)
+) %>%  
+  Map$addLayers(visParams = list(palette = cpt(pal = "ncl_BlueYellowRed")))
 
-write.csv(new_links,"data/drones.csv")
+
+# 3. New URLS -------------------------------------------------------------
+
+new_links <- tibble(
+  villages = c(
+    'llanchama',
+    '12 de abril',
+    '23 de febrero',
+    'quistococha',
+    'san carlos',
+    'el paujil',
+    'san lucas',
+    'zungarococha',
+    'cahuide 01',
+    'cahuide 02',
+    'cahuide 03',
+    'el varillal 01',
+    'el varillal 02',
+    'el varillal 03'),
+  rgb = rgb$rgee$tokens,
+  thermal = thermal$rgee$tokens
+  )
+
+write_csv(new_links,"data/drones.csv")
+rsconnect::deployApp(appName = "droneapp")
